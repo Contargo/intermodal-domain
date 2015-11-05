2079daf9dd84a4732da05aa6df062af370c81867
Olle Törnström <toernstroem@synyx.de>
2015-11-05 16:36:31 +0100

    Add main package description
    * Adding a placeholder interface, allowing for a preview of the
      generated Javadoc to be generated.

---
+++ src/main/java/net/contargo/cola/COLA.java
@@ -0,0 +1,11 @@
+package net.contargo.cola;
+
+/**
+ * --PLACEHOLDER CLASS. REMOVE THIS AS SOON AS POSSIBLE.--
+ *
+ * @author  Olle Törnström - toernstroem@synyx.de
+ */
+public interface COLA {
+
+    // OK
+}
---
+++ src/main/java/net/contargo/cola/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contargo Open Logistics Applications (COLA) describes the system and platform for applications in the logistics
+ * business domain. COLA integrates and enables solutions that typically handle intermodal container, and container
+ * shipping.
+ *
+ * @author  Olle Törnström - toernstroem@synyx.de
+ */
+package net.contargo.cola;
