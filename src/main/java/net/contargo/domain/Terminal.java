c6653fcf44dc182c0bf851ca3f3b45f90612b2f0
Olle Törnström <toernstroem@synyx.de>
2015-11-09 15:44:38 +0100

    Amend terminal description

--- src/main/java/net/contargo/domain/Terminal.java
+++ src/main/java/net/contargo/domain/Terminal.java
@@ -1,14 +1,14 @@
 package net.contargo.domain;
 
 /**
- * A station, or junction, along a transport route - typically along the inland (<em>hinterland</em>), waterways, rivers
- * or canals.
+ * A station, or junction, along a transport route - typically along the inland (<em>hinterland</em>) waterways, rivers
+ * or canals. Each {@link Terminal} is among other things specified by it's {@link Site} and {@link Location}.
  *
  * <p>A {@link Terminal} is in our domain very often an acting business unit and a subsidiary of the Contargo
- * corporation. This typically means that the {@link Terminal} has an identity and is referred to, not only as a
- * geographical location, as an <i>actor</i> in the system. For example, business events and messages typically stems
- * from one of many {@link Terminal terminals} and roles or groups are also often bound to a specific {@link Terminal}.
- * </p>
+ * corporation. This typically means that the {@link Terminal}, as a organizational unit, has an identity and is
+ * referred to, not only as a geographical location, but also as an <i>actor</i> in the system. For example, business
+ * events and messages typically stems from one of many {@link Terminal terminals} and roles or groups are also often
+ * bound to a specific {@link Terminal}.</p>
  *
  * <p>In german {@link Hinterlandterminal}, but also simply {@link Terminal}.</p>
  *
