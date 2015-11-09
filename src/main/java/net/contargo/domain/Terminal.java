a3e19484dc7d6f77d50fc41c49895fbf02a34781
Olle Törnström <toernstroem@synyx.de>
2015-11-09 15:08:20 +0100

    Update terminal description

--- src/main/java/net/contargo/domain/Terminal.java
+++ src/main/java/net/contargo/domain/Terminal.java
@@ -1,9 +1,16 @@
 package net.contargo.domain;
 
 /**
- * A {@link Site} and {@link Location} in <em>hinterland</em> - especially along the inland waterways, rivers or canals.
+ * A station, or junction, along a transport route - typically along the inland (<em>hinterland</em>), waterways, rivers
+ * or canals.
  *
- * <p>In german {@link Hinterlandterminal}.</p>
+ * <p>A {@link Terminal} is in our domain very often an acting business unit and a subsidiary of the Contargo
+ * corporation. This typically means that the {@link Terminal} has an identity and is referred to, not only as a
+ * geographical location, as an <i>actor</i> in the system. For example, business events and messages typically stems
+ * from one of many {@link Terminal terminals} and roles or groups are also often bound to a specific {@link Terminal}.
+ * </p>
+ *
+ * <p>In german {@link Hinterlandterminal}, but also simply {@link Terminal}.</p>
  *
  * @author  Olle Törnström - toernstroem@synyx.de
  */
