01697dfff62ca12f998dd986b5ad6aa2a7ee4ff8
Aljona Murygina <murygina@synyx.de>
2016-06-23 10:03:11 +0200

    Fix typo

--- src/main/java/net/contargo/domain/Event.java
+++ src/main/java/net/contargo/domain/Event.java
@@ -4,7 +4,7 @@ package net.contargo.domain;
  * A happening, pertaining to some business occasion, typically posted by a component of the system that is published
  * into the open messaging domain, not being addressed to a specific recipient.
  *
- * <p>In german {@link Ereignins}</p>
+ * <p>In german {@link Ereignis}</p>
  *
  * @author  Olle Törnström - toernstroem@synyx.de
  */
@@ -17,5 +17,5 @@ public interface Event {
  * Nutzer/Rollen über diese Ereignisse informiert werden, so müssen die einzelnen Systeme Nachrichten an die Nutzer
  * versenden.
  */
-interface Ereignins {
+interface Ereignis {
 }
