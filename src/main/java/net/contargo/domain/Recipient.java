f53db480c4694c7851a7850c1d2529a740efe736
Olle Törnström <toernstroem@synyx.de>
2017-11-14 10:19:15 +0100

    Add recipient type

---
+++ src/main/java/net/contargo/domain/Recipient.java
@@ -0,0 +1,18 @@
+package net.contargo.domain;
+
+/**
+ * Reciever of notifications and messages.
+ *
+ * <p>A generalized and well known type that is present in most information systems. The benefit of defining this type
+ * is first and foremost to separate it from users or profiles, in that it must not contain the same rich information.
+ * A recipient could be as simple as a addressable entity, for example a user identifier and an email address.</p>
+ *
+ * <p>In german {@link Empfaenger}.</p>
+ *
+ * @author  Olle Törnström - toernstroem@synyx.de
+ */
+public interface Recipient {
+}
+
+interface Empfaenger {
+}
