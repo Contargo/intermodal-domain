c44a9f055820de3140bb9ae856d2fd86debaa412
Isabell Duerlich <duerlich@synyx.de>
2018-04-16 12:17:20 +0200

    adds further documentation to status data

--- src/main/java/api/statusData/MeansOfTransportationStatus.java
+++ src/main/java/api/statusData/MeansOfTransportationStatus.java
@@ -4,9 +4,25 @@ import java.util.Date;
 
 
 /**
- * Status of means of transport for communication between agents.
+ * Status of means of transport for communication flow of intermodal freight transport.
  *
- * <p>In german Verkehrsmittelstatus</p>
+ * <p>DIGIT_name: Verkehrsmittelstatus</p>
+ *
+ * <p>DIGIT_english: means of transportation status</p>
+ *
+ * <p>DIGIT_definition: Verkehrsmittelstatus für den Informationsfluss in der intermodalen Kette. Über den
+ * Verkehrsmittelstatus und den {@link api.statusData.StatusLU Ladeeinheitenstatus} kann ein reibungsloser Austausch
+ * zwischen den Akteuren sichergestellt werden. Prinzipiell sollten die beiden Status als Paket verstanden werden. Die
+ * Kerndaten sind jeweils immer gleich und werden durch den jeweiligen Status konkretisiert. Dabei gibt es zwei Blöcke
+ * pro Statuspaket. Zum einen die Identifikation des Handelnden inklusive Begleitinformationen und zum anderen die
+ * Konkretisierung was genau zu welchem Zeitpunkt passiert. Begleitinformationen beim Verkehrsmittel betreffen die
+ * Transportrelation.</p>
+ *
+ * <p>DIGIT_definition_english: Status of means of transport for communication flow of intermodal freight transport.
+ * The means of transportation status and {@link api.statusData.StatusLU loading unit status} ensure a fluent exchange
+ * between actors. Both statuses should be seen as package. Their core data is similar and is concretized by their
+ * status. There are two blocks per status package. On one hand the identification of the doer on the other hand the
+ * schedule of the different operational steps.</p>
  *
  * @author  Isabell Dürlich - duerlich@synyx.de
  */
--- src/main/java/api/statusData/StatusLU.java
+++ src/main/java/api/statusData/StatusLU.java
@@ -1,9 +1,25 @@
 package api.statusData;
 
 /**
- * Describes the status of a {@link api.masterData.loadingUnit.LoadingUnit} for communication between agents.
+ * Status of loading unit for communication flow of intermodal freight transport.
  *
- * <p>In german Ladeeinheitenstatus</p>
+ * <p>DIGIT_name: Ladeeinheitenstatus</p>
+ *
+ * <p>DIGIT_english: loading unit status</p>
+ *
+ * <p>DIGIT_definition: Ladeeinheitenstatus für den Informationsfluss in der intermodalen Kette. Über den
+ * Ladeeinheitenstatus und den {@link api.statusData.MeansOfTransportationStatus Verkehrsmittelstatus} kann ein
+ * reibungsloser Austausch zwischen den Akteuren sichergestellt werden. Prinzipiell sollten die beiden Status als Paket
+ * verstanden werden. Die Kerndaten sind jeweils immer gleich und werden durch den jeweiligen Status konkretisiert.
+ * Dabei gibt es zwei Blöcke pro Statuspaket. Zum einen die Identifikation des Handelnden inklusive
+ * Begleitinformationen und zum anderen die Konkretisierung was genau zu welchem Zeitpunkt passiert.
+ * Begleitinformationen bei der Ladeeinheit betreffen beispielsweise den Auftrag.</p>
+ *
+ * <p>DIGIT_definition_english: Status of means of transport for communication flow of intermodal freight transport.
+ * The loading unit status and {@link api.statusData.MeansOfTransportationStatus means of transportation status} ensure
+ * a fluent exchange between actors. Both statuses should be seen as package. Their core data is similar and is
+ * concretized by their status. There are two blocks per status package. On one hand the identification of the doer on
+ * the other hand the schedule of the different operational steps.</p>
  *
  * @author  Isabell Dürlich - duerlich@synyx.de
  */
