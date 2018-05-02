c029e12cd1810400ea0e55387d02ed03cc51ea89
Isabell Duerlich <duerlich@synyx.de>
2018-05-02 12:35:52 +0200

    moves package info to intermodal domain folder

--- src/main/java/net/contargo/intermodal/domain/package-info.java
+++ src/main/java/net/contargo/intermodal/domain/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Defines the business domain language and expressions as tagging interfaces, that may be applied to solutions for
- * reference and correlation.
+ * Defines the business net.contargo.domain language and expressions as tagging interfaces, that may be applied to
+ * solutions for reference and correlation.
  *
  * @author  Olle Törnström - toernstroem@synyx.de
  */
