e4845ac0b174ddff9cbc0dce1bdb3283b6677336
Isabell Duerlich <duerlich@synyx.de>
2018-05-02 12:22:57 +0200

    fixes wrong month in iso date

--- src/main/java/net/contargo/intermodal/domain/utility/ISO8601DateFormatter.java
+++ src/main/java/net/contargo/intermodal/domain/utility/ISO8601DateFormatter.java
@@ -18,7 +18,7 @@ public class ISO8601DateFormatter {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
         simpleDateFormat.setTimeZone(utc);
 
-        Date time = new GregorianCalendar(year, month + 1, day, 1, 0).getTime();
+        Date time = new GregorianCalendar(year, month - 1, day, 1, 0).getTime();
 
         return simpleDateFormat.format(time);
     }
