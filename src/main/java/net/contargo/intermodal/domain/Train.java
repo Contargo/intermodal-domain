6bd4a3679a73c1578e72ad0aa458f2e3fbb89ec9
Isabell Duerlich <duerlich@synyx.de>
2018-09-12 09:58:16 +0200

    adds train placeholder

---
+++ src/main/java/net/contargo/intermodal/domain/Train.java
@@ -0,0 +1,10 @@
+package net.contargo.intermodal.domain;
+
+/**
+ * Placeholder for a train as means of transport.
+ *
+ * @author  Isabell Dürlich - duerlich@synyx.de
+ * @source  Contargo specific
+ */
+public class Train implements MeansOfTransport {
+}
